6dbd1679ab15ba95f0ed6bee1186f515fa795170
Pawel Psztyc <jarrodek@gmail.com>
2019-03-25 12:38:19 -0700

    Update: Updating import paths to match web spec

--- arc-icons.js
+++ arc-icons.js
@@ -13,9 +13,8 @@ the License.
 */
 import '../../@polymer/iron-icon/iron-icon.js';
 import '../../@polymer/iron-iconset-svg/iron-iconset-svg.js';
-const $_documentContainer = document.createElement('template');
-
-$_documentContainer.innerHTML = `<iron-iconset-svg size="24" name="arc">
+const $documentContainer = document.createElement('template');
+$documentContainer.innerHTML = `<iron-iconset-svg size="24" name="arc">
 <svg>
   <defs>
     <g id="account-circle"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 3c1.66 0 3 1.34 3 3s-1.34 3-3 3-3-1.34-3-3 1.34-3 3-3zm0 14.2c-2.5 0-4.71-1.28-6-3.22.03-1.99 4-3.08 6-3.08 1.99 0 5.97 1.09 6 3.08-1.29 1.94-3.5 3.22-6 3.22z"></path></g>
@@ -102,4 +101,4 @@ $_documentContainer.innerHTML = `<iron-iconset-svg size="24" name="arc">
   </defs></svg>
 </iron-iconset-svg>`;
 
-document.head.appendChild($_documentContainer.content);
+document.head.appendChild($documentContainer.content);
