4c4741652c25f7aae03214e1b679edb23a0d3fee
Alex Perez <alexperez@mulesoft.com>
2025-07-22 18:05:49 -0300

    chore: update package.json and web-test-runner configuration for improved testing setup, including dependency version changes and timeout settings

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -1,12 +1,25 @@
+import { playwrightLauncher } from '@web/test-runner-playwright';
+
 export default {
-  files: 'test/**/*.test.js',
   nodeResolve: true,
-  middleware: [
-    function rewriteBase(context, next) {
-      if (context.url.indexOf('/base') === 0) {
-        context.url = context.url.replace('/base', '');
-      }
-      return next();
-    }
+  coverage: true,
+  browsers: [
+    playwrightLauncher({ product: 'chromium' }),
+    playwrightLauncher({ product: 'firefox' }),
+    playwrightLauncher({
+      product: 'webkit',
+      launchOptions: {
+        headless: true,
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      },
+    }),
   ],
+  testFramework: {
+    config: {
+      timeout: '10000',
+    },
+  },
+  browserStartTimeout: 60000,
+  testsStartTimeout: 60000,
+  testsFinishTimeout: 60000,
 };
